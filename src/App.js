0b36f65efedbcea129fe3e9585d6bebfcdbb173b
Shashwat <shashwat@Shashwats-MacBook-Pro-2.local>
2024-06-13 19:24:52 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+let data = [
+
+  { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." }
+
+];
+
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [display, setDisplay] = useState("");
+
+  const handleChange = (e) =>{
+    setSearch(e.target.value);
+  }
+
+  const handleClick = () =>{
+    setDisplay('Word not found in the dictionary');
+   let ans = data.find((ele)=> ele.word.toLowerCase()===search.toLowerCase());
+   if(ans){
+    setDisplay(ans.meaning)
+   }
+  //  console.log(display)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Dictionary App</h1>
+    <input type="text" placeholder="Search for a word..." onChange={handleChange} value={search}/>
+    <button onClick={handleClick}>Search</button>
+    <div><b>Definition:</b></div>
+    <p>{display}</p>
+    </>
   );
 }
 

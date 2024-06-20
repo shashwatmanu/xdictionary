fd2910f774d9a17d11c98fce50b554eafb77a54d
Shashwat <shashwat@Shashwats-MacBook-Pro-2.local>
2024-06-20 13:07:32 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -2,41 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
 
-let data = [
 
-  { word: "React", meaning: "A JavaScript library for building user interfaces." },
-
-  { word: "Component", meaning: "A reusable building block in React." },
-
-  { word: "State", meaning: "An object that stores data for a component." }
-
-];
 
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [display, setDisplay] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+  const [wordMeaning, setWordMeaning] = useState("");
 
   const handleChange = (e) =>{
-    setSearch(e.target.value);
+    setSearchWord(e.target.value);
+  }
+
+  const findInDictionary = () =>{
+    let data = [
+
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+    
+      { word: "Component", meaning: "A reusable building block in React." },
+    
+      { word: "State", meaning: "An object that stores data for a component." }
+    
+    ];
+    
+    let ans = data.find((ele)=> ele.word.toLowerCase()===searchWord.toLowerCase());
+    if(ans){
+     setWordMeaning(ans.meaning)
+    }
+    else{
+      setWordMeaning('Word not found in the dictionary');
+    }
   }
 
   const handleClick = () =>{
-    setDisplay('Word not found in the dictionary');
-   let ans = data.find((ele)=> ele.word.toLowerCase()===search.toLowerCase());
-   if(ans){
-    setDisplay(ans.meaning)
-   }
-  //  console.log(display)
+    findInDictionary();
   }
 
   return (
     <>
     <h1>Dictionary App</h1>
-    <input type="text" placeholder="Search for a word..." onChange={handleChange} value={search}/>
+    <input type="text" placeholder="Search for a word..." onChange={handleChange} value={searchWord}/>
     <button onClick={handleClick}>Search</button>
     <div>Definition:</div>
-    <div>{display}</div>
+    <div>{wordMeaning}</div>
     
     </>
   );
